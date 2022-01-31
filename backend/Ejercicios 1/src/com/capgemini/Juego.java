7ad5003813507a6d20f95f5bfff04732a084df48
[OscarSanDo] <[oscar.sanchez-domingo@capgemini.com]>
2022-01-31 13:29:33 +0100

    Actualización JuegoDelNumero

---
+++ backend/Ejercicios 1/src/com/capgemini/Juego.java
@@ -0,0 +1,68 @@
+package com.capgemini;
+
+import java.util.Random;
+
+public class Juego {
+	
+	private int contador = 10;
+	private Random random = new Random();
+	private int numeroRandom = random.nextInt(101);
+	private String mensaje = "";
+	private boolean finalizarJuego = false;
+
+	public Juego() {
+		inicializar();
+		}
+
+	public void inicializar() {
+	contador = 10;
+	numeroRandom = random.nextInt(101);
+	mensaje = "";
+	finalizarJuego = false;
+	}
+
+
+
+	public void jugada(int num) {
+
+
+
+	if (num > numeroRandom) {
+	mensaje = "El número secreto es MENOR que " + num + ", te quedan " + (contador - 1) + " intentos.";
+	} else if (num < numeroRandom) {
+	mensaje = "El número secreto es MAYOR que " + num + ", te quedan " + (contador - 1) + " intentos.";
+	} else {
+	mensaje = "Has acertado el número secreto, has necesitado " + (10 - (contador - 1)) + " intentos.";
+
+
+
+	finalizarJuego = true;
+	return;
+	}
+	contador--;
+
+
+
+	if (contador == 0) {
+	mensaje = "No te quedan intentos, has perido la partida. El número secreto era: "   + num;
+
+
+
+	finalizarJuego = true;
+	}
+	}
+
+
+
+	public String getMensaje() {
+	return mensaje;
+	}
+
+
+
+	public boolean getTerminado() {
+	return finalizarJuego;
+	}
+	
+
+}
--- backend/Ejercicios 1/src/com/capgemini/JuegoDelNumero.java
+++ backend/Ejercicios 1/src/com/capgemini/JuegoDelNumero.java
@@ -8,44 +8,34 @@ public class JuegoDelNumero {
 
 	public static void main(String[] args) {
 
-        int intentos = 10;
-        int numeroRandom = Inicializar();
-        System.out.println(numeroRandom);
+			comenzarJuego();
+			}
 
-        do {
-            intentos -= 1;
-            boolean aciertoJugada = jugada(numeroRandom, intentos);
-            if ( aciertoJugada ){
-                break;
-            };
-        }while ( intentos > 0);
-       
-        if (intentos == 0) {
-        JOptionPane.showMessageDialog(null, "Te has quedado sin intentos, has perdido la partida");}
+	
+	public static void comenzarJuego() {
+		
+		int numeroUsuario;
+		Juego juego = new Juego();
+		
 
-    }
+		do {
 
-    private static boolean jugada(int numeroRandom, int intentos){
+		numeroUsuario = Integer.parseInt(JOptionPane.showInputDialog(null,
+		"Adivina el número secreto que está entre 0 Y 100, \ntienes 10 intentos para acertarlo, introduce un número "));
+		juego.jugada(numeroUsuario);
 
-        String resp = JOptionPane.showInputDialog(null, "Introduce un número entre 0 y 100:");
-  
-        int cadUsuario = Integer.parseInt(resp);
+		JOptionPane.showMessageDialog(null, juego.getMensaje());
 
-        if (cadUsuario == numeroRandom) {
-            JOptionPane.showMessageDialog(null, "Has Acertado el Número ");
-            return true;
-        } else if (cadUsuario > numeroRandom) {
-            JOptionPane.showMessageDialog(null, "El numero Random es menor, te quedan " + intentos + " intentos: ");
-        } else if (cadUsuario < numeroRandom) {
-            JOptionPane.showMessageDialog(null, "El numero Random es mayor, te quedan " + intentos + " intentos: ");
-        }
-        return false;
-        
-    }
+		} while (!juego.getTerminado());
 
-    private static int Inicializar(){
-        return new Random().nextInt(101); 
-    }
+	}
 }
+	
+	
+	
+	
+	
+	
+
 
 

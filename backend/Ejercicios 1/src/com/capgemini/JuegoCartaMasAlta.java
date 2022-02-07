0827427ca775450c6acf412306d96d4848e41967
[OscarSanDo] <[oscar.sanchez-domingo@capgemini.com]>
2022-02-07 11:03:58 +0100

    Actualización control número erroneo

--- backend/Ejercicios 1/src/com/capgemini/JuegoCartaMasAlta.java
+++ backend/Ejercicios 1/src/com/capgemini/JuegoCartaMasAlta.java
@@ -18,22 +18,34 @@ public class JuegoCartaMasAlta {
 		BarajaCartas barajaNueva = new BarajaCartas(TipoJuego.POKER);
 		barajaNueva.barajar();
 
+		
 		jugador1 = Integer.parseInt(
-				JOptionPane.showInputDialog(null, "Escoge una carta de la baraja (de la posicion 0 a la 51)"));
-
-	
-
-		JOptionPane.showMessageDialog(null, (barajaNueva.baraja.get(jugador1).getEtiqueta() + " de "
-				+ barajaNueva.baraja.get(jugador1).getPalos()));
-
+				JOptionPane.showInputDialog(null, "Escoge una carta de la baraja (de la posicion 1 a la 52)")) - 1;
+		if (jugador1 < 0 || jugador1 >= 52) {
+			jugador1 = 0;
+			JOptionPane.showMessageDialog(null,
+					"El número introducido no es correcto se te asignará por defecto la primera carta del mazo");
+			JOptionPane.showMessageDialog(null,
+					(barajaNueva.baraja.get(0).getEtiqueta() + " de " + barajaNueva.baraja.get(0).getPalos()));
+		} else {
+			JOptionPane.showMessageDialog(null, (barajaNueva.baraja.get(jugador1).getEtiqueta() + " de "
+					+ barajaNueva.baraja.get(jugador1).getPalos()));
+		}
+		
+		
 		jugador2 = Integer.parseInt(
-				JOptionPane.showInputDialog(null, "Escoge una carta de la baraja (de la posicion 0 a la 51)"));
+				JOptionPane.showInputDialog(null, "Escoge una carta de la baraja (de la posicion 1 a la 52)")) - 1;
+		if (jugador2 < 0 || jugador2 >= 52 || jugador2 == jugador1) {
+			jugador2 = 51;
+			JOptionPane.showMessageDialog(null,
+					"El número introducido no es correcto se te asignará por defecto la última carta del mazo");
+			JOptionPane.showMessageDialog(null,
+					(barajaNueva.baraja.get(51).getEtiqueta() + " de " + barajaNueva.baraja.get(51).getPalos()));
+		} else {
+			JOptionPane.showMessageDialog(null, (barajaNueva.baraja.get(jugador2).getEtiqueta() + " de "
+					+ barajaNueva.baraja.get(jugador2).getPalos()));
+		}
 
-		JOptionPane.showMessageDialog(null, (barajaNueva.baraja.get(jugador2).getEtiqueta() + " de "
-				+ barajaNueva.baraja.get(jugador2).getPalos()));
-
-		// barajaNueva.mostrarBaraja(); //muestra baraja para comprobar que los indices
-		// corresponden
 
 		barajaNueva.compararValorCarta(barajaNueva.baraja.get(jugador1), barajaNueva.baraja.get(jugador2));
 

d204850e2e24fd70d2e4c33ec98fabcf5efb320e
OscarSanDo <oscar.sanchez-domingo@capgemini.com>
2022-03-03 15:30:48 +0100

    Servicio rental con ShortDTO

--- backend/demos.spring/src/main/java/com/example/domains/entities/Rental.java
+++ backend/demos.spring/src/main/java/com/example/domains/entities/Rental.java
@@ -3,10 +3,19 @@ package com.example.domains.entities;
 import java.io.Serializable;
 import javax.persistence.*;
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 
+import org.hibernate.annotations.Generated;
+import org.hibernate.annotations.GenerationTime;
+
+import com.example.domains.core.entities.EntityBase;
+
+import java.util.ArrayList;
 import java.util.Date;
 import java.sql.Timestamp;
 import java.util.List;
+import java.util.Objects;
 
 
 /**
@@ -16,7 +25,7 @@ import java.util.List;
 @Entity
 @Table(name="rental")
 @NamedQuery(name="Rental.findAll", query="SELECT r FROM Rental r")
-public class Rental implements Serializable {
+public class Rental extends EntityBase<Rental> implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
@@ -25,10 +34,12 @@ public class Rental implements Serializable {
 	private int rentalId;
 
 	@Column(name="last_update")
+	@Generated(value = GenerationTime.ALWAYS)
 	private Timestamp lastUpdate;
 
 	@Temporal(TemporalType.TIMESTAMP)
 	@Column(name="rental_date")
+	@NotNull
 	private Date rentalDate;
 
 	@Temporal(TemporalType.TIMESTAMP)
@@ -36,26 +47,52 @@ public class Rental implements Serializable {
 	private Date returnDate;
 
 	//bi-directional many-to-one association to Payment
-	@OneToMany(mappedBy="rental")
+	@OneToMany(mappedBy="rental", cascade = CascadeType.ALL, orphanRemoval = true)
 	@Valid
 	private List<Payment> payments;
 
 	//bi-directional many-to-one association to Customer
 	@ManyToOne
+	@Positive
+	@NotNull
 	@JoinColumn(name="customer_id")
 	private Customer customer;
 
 	//bi-directional many-to-one association to Inventory
 	@ManyToOne
+	@Positive
+	@NotNull
 	@JoinColumn(name="inventory_id")
 	private Inventory inventory;
 
 	//bi-directional many-to-one association to Staff
 	@ManyToOne
+	@Positive
+	@NotNull
 	@JoinColumn(name="staff_id")
 	private Staff staff;
 
 	public Rental() {
+		super();
+		payments = new ArrayList<Payment>();
+	}
+	
+	public Rental(int rentalId) {
+		this();
+		this.rentalId = rentalId;
+	}
+	
+	public Rental(int rentalId, @NotNull Date rentalDate, Date returnDate, @Positive @NotNull Customer customer, 
+			@Positive @NotNull Inventory inventory, @Positive @NotNull Staff staff ) {
+		this();
+		this.rentalId = rentalId;
+		this.rentalDate = rentalDate;
+		this.returnDate = returnDate;
+		this.customer = customer;
+		this.inventory = inventory;
+		this.staff = staff;
+		
+		
 	}
 
 	public int getRentalId() {
@@ -96,14 +133,17 @@ public class Rental implements Serializable {
 
 	public void setPayments(List<Payment> payments) {
 		this.payments = payments;
+		
 	}
 
 	public Payment addPayment(Payment payment) {
 		getPayments().add(payment);
 		payment.setRental(this);
+		payment.setCustomer(getCustomer());
 
 		return payment;
 	}
+	
 
 	public Payment removePayment(Payment payment) {
 		getPayments().remove(payment);
@@ -135,5 +175,26 @@ public class Rental implements Serializable {
 	public void setStaff(Staff staff) {
 		this.staff = staff;
 	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(rentalId);
+	}
 
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!(obj instanceof Rental))
+			return false;
+		return rentalId == ((Rental) obj).rentalId;
+	}
+
+	@Override
+	public String toString() {
+		return "Rental [rentalId=" + rentalId + ", rentalDate=" + rentalDate + ", returnDate=" + returnDate
+				+ ", payments=" + payments + ", customer=" + customer + ", inventory=" + inventory + ", staff=" + staff
+				+ "]";
+	}
+	
+	
 }

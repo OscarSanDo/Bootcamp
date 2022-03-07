d427c010adac4e80764766aa67eaea3250b57af0
OscarSanDo <oscar.sanchez-domingo@capgemini.com>
2022-03-07 20:00:06 +0100

    cambios 07/03

--- backend/demos.spring/src/main/java/com/example/Application.java
+++ backend/demos.spring/src/main/java/com/example/Application.java
@@ -2,6 +2,8 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 import springfox.documentation.oas.annotations.EnableOpenApi;
 import com.example.application.dtos.ActorDTO;
 import com.example.application.dtos.ActorShortDTO;
@@ -27,6 +29,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 @EnableOpenApi
+@EnableEurekaClient
 @SpringBootApplication
 public class Application implements CommandLineRunner { 
 

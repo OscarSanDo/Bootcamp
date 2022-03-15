58da4bc20133b7913b42785fbc2b9c3a31efae72
OscarSanDo <oscar.sanchez-domingo@capgemini.com>
2022-03-15 17:23:16 +0100

    Actividad 1 completada

---
+++ frontend/actividadesJS/actividadesJS.js
@@ -0,0 +1,16 @@
+
+
+//Actividad 1
+
+function devuelveNumeroRDM(min, max) {
+    return Math. trunc((Math.random() * (101 - 1) + 1));
+  }
+
+
+let numRdm = devuelveNumeroRDM();
+console.log(numRdm);
+alert("pulsa el boton para generar un número aleatorio");
+alert(`El número Ramdom es ` + numRdm);
+
+//Actividad 2
+

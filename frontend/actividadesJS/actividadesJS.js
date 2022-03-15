28b3c07c6c5f31454e7871dc20ce4a7e9cf6457c
OscarSanDo <oscar.sanchez-domingo@capgemini.com>
2022-03-15 17:52:55 +0100

    actualización actividadades

--- frontend/actividadesJS/actividadesJS.js
+++ frontend/actividadesJS/actividadesJS.js
@@ -1,49 +1,52 @@
 
 
-//Actividad 1
+// Actividad 1
 
 function devuelveNumeroRDM(min, max) {
-    return Math. trunc((Math.random() * (101 - 1) + 1));
+    return Math. trunc((Math.random() * (max - min) + min));
   }
 
 
-//let numRdm = devuelveNumeroRDM();
-// console.log(numRdm);
-// alert("pulsa el boton para generar un número aleatorio");
-// alert(`El número Ramdom es ` + numRdm);
+let numRdm = devuelveNumeroRDM(1, 100);
+console.log(numRdm);
+alert("pulsa el boton para generar un número aleatorio");
+alert(`El número Ramdom es ` + numRdm);
 
 
 //Actividad 2
 
 
 
-		let intentos = 10; // variable para guardar el n�mero de intentos
-		let cadUsuario;
-		let numeroRandom = devuelveNumeroRDM(); // genera el numero aleatorio
+		// let intentos = 10; // variable para guardar el n�mero de intentos
+		// let cadUsuario;
+		// let numeroRandom = devuelveNumeroRDM(); // genera el numero aleatorio
 
-		alert("Adivina el número entre 0 y 100");
-		alert("Tienes 10 intentos.");
-		//System.out.println(numeroRandom);
+		// alert("Adivina el número entre 0 y 100");
+		// alert("Tienes 10 intentos.");
+		// //System.out.println(numeroRandom);
 
-		do {
-			cadUsuario = parseInt(prompt("Introduce un número entre 0 y 100: "));
+		// do {
+		// 	cadUsuario = parseInt(prompt("Introduce un número entre 0 y 100: "));
 			
 		
 			
-			if (cadUsuario === numeroRandom) {
-				alert("Has acertado");
-				break;
+		// 	if (cadUsuario === numeroRandom) {
+		// 		alert("Has acertado");
+		// 		break;
 
-			} else if (cadUsuario > numeroRandom) {
-				alert("El número Random es menor, te quedan " + --intentos + " intentos: ");
+		// 	} else if (cadUsuario > numeroRandom) {
+		// 		alert("El número Random es menor, te quedan " + --intentos + " intentos: ");
 
 
-			} else if (cadUsuario < numeroRandom) {
-				alert("El número Random es mayor, te quedan " + --intentos + " intentos: ");
-			}
+		// 	} else if (cadUsuario < numeroRandom) {
+		// 		alert("El número Random es mayor, te quedan " + --intentos + " intentos: ");
+		// 	}
 
-		} while (intentos > 0);
+		// } while (intentos > 0);
 		
-		if (intentos == 0) {
-      alert("No te quedan intentos, has perdido la partida");}
+		// if (intentos == 0) {
+    //   alert("No te quedan intentos, has perdido la partida");}
 	
+
+    //Actividad 3
+

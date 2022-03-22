a2ba7f050070688dd256abcd5ee0cf9ef937e710
OscarSanDo <oscar.sanchez-domingo@capgemini.com>
2022-03-22 16:19:35 +0100

    cambios proyecto prueba Angular

--- pruebasAngular/src/environments/environment.ts
+++ pruebasAngular/src/environments/environment.ts
@@ -2,8 +2,11 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+import { ERROR_LEVEL } from "src/lib/my-core/services/logger.service";
+
 export const environment = {
-  production: false
+  production: false,
+  ERROR_LEVEL: 99,
 };
 
 /*
